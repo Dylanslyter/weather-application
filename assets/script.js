1f3d52320325c479db70fceaa82a27d67c93d9de
dylan <dylan.slyter@yahoo.com>
2024-03-17 20:21:07 -0400

    got the icons onto the days of the week

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
 const apiKey = 'ea6b0dd025b3bb4d331b523df25926f7'; // Replace with your API key
 
 document.getElementById('search-form').addEventListener('submit', function(event) {
@@ -28,8 +30,13 @@ function fetchForecast(city) {
 
 function displayCurrentWeather(data) {
   // Display current weather data
+  
+  console.log(data.weather[0].main)
+  const weatherStatis = data.weather[0].icon
   const weatherDisplay = document.getElementById('weather-display');
   weatherDisplay.innerHTML = `
+  <img src = "https://openweathermap.org/img/wn/${weatherStatis}@2x.png"/>
+  
     <h2>${data.name}</h2>
     <p>Date: ${new Date().toLocaleDateString()}</p>
     <p>Temperature: ${data.main.temp}°C</p>
@@ -40,12 +47,15 @@ function displayCurrentWeather(data) {
 
 function displayForecast(data) {
   // Display 5-day forecast
+  console.log(data)
   const forecastDisplay = document.getElementById('forecast-display');
   forecastDisplay.innerHTML = '';
   for (let i = 0; i < data.list.length; i += 8) {
     const forecast = data.list[i];
+    const weatherStatis = forecast.weather[0].icon
     const forecastItem = `
       <div class="forecast-item">
+      <img src = "https://openweathermap.org/img/wn/${weatherStatis}@2x.png"/>
         <p>Date: ${forecast.dt_txt.slice(0, 10)}</p>
         <p>Temperature: ${forecast.main.temp}°C</p>
         <p>Humidity: ${forecast.main.humidity}%</p>
@@ -56,12 +66,12 @@ function displayForecast(data) {
   }
 }
 
-function addToSearchHistory(city) {
+
+  function addToSearchHistory(city) {
   // Add city to search history
-  const searchHistory = document.getElementById('search-history');
-  const searchHistoryItem = document.createElement('span');
-  searchHistoryItem.classList.add('search-history-item');
-  searchHistoryItem.textContent = city;
-  searchHistoryItem.addEventListener('click', () => fetchWeather(city));
-  searchHistory.appendChild(searchHistoryItem);
-}
+   searchHistory.push(city);
+   // Update local storage with the updated search history
+   localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+  
+  }
